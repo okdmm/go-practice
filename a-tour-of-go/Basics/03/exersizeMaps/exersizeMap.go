27e1334868d69ed379f387b0ef61d918e5d77135
okada <kirinnremonn1122@gmail.com>
2019-01-14 09:49:43 +0900

    better

--- a-tour-of-go/Basics/03/exersizeMaps/exersizeMap.go
+++ a-tour-of-go/Basics/03/exersizeMaps/exersizeMap.go
@@ -6,9 +6,6 @@ func WordCount(s string) map[string]int {
 	words := strings.Split(s, " ")
 
 	wc := make(map[string]int)
-	for _, word := range words {
-		wc[word] = 0
-	}
 
 	for _, word := range words {
 		wc[word] += 1
